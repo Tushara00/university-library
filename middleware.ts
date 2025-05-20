747ed0d5b8b2b4c5e3e2fe418d60ce42c789165b
Tushara00 <ahnafmon6@gmail.com>
2025-05-20 13:58:48 +0600

    dhjhf

--- middleware.ts
+++ middleware.ts
@@ -18,5 +18,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|icons|api/auth|imageKIt).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|icons|images|api/auth|imageKIt).*)"],
 };
